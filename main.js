8965dc5440cae45580e5057edd87407208ce0abb
Chokerino <43617111+Chokerino@users.noreply.github.com>
2018-10-06 09:29:39 +0530

    ALMOST

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var instruct = document.getElementById("insBtn");
 var pop = document.getElementById("Pg");
 
@@ -55,6 +56,8 @@ function rules3(){
         window.addEventListener('click', outsideClick3);
 }
 var modal = document.getElementById('myModal');
+var modala = document.getElementById('myModala');
+var modalb = document.getElementById('myModalb');
 
 var btn = document.getElementById("meme1");
 
@@ -69,11 +72,11 @@ btn.onclick = function() {
     modal.style.display = "block";
 }
 btn1.onclick = function() {
-    modal.style.display = "block";
+    modala.style.display = "block";
 }
 
 btn2.onclick = function() {
-    modal.style.display = "block";
+    modalb.style.display = "block";
 }
 
 
@@ -89,4 +92,168 @@ window.onclick = function(event) {
     }
 }
 
+var video = document.querySelector('iframe');
 
+var result1 = document.getElementById("opt1");
+var dispc = document.getElementById("Resultr");
+var dispi = document.getElementById("Resultw");
+var result2 = document.getElementById("opt2");
+
+result1.addEventListener('click', out1);
+result2.addEventListener('click', out2);
+
+function outsideClick4(e){
+  if(e.target == Resultr){
+    dispc.style.display = 'none';
+  }
+}
+function outsideClick5(e){
+  if(e.target == Resultw){
+    dispi.style.display = 'none';
+  }
+}
+var correct =0;
+var incorrect =0;
+function out1(){
+        correct = correct + 1;
+        dispc.style.display = 'block';
+        window.addEventListener('click', outsideClick4);
+        modal.style.display = "none";
+        var mytry = video.getAttribute('src');
+         if(correct === 1) {
+         video.setAttribute ("src","https://www.youtube.com/embed/xz3PbpPR6DY?autoplay=1");
+         }    else if(correct ===2) {
+         video.setAttribute ('src','https://www.youtube.com/embed/0HgNdPOZyvQ?autoplay=1');
+        }
+        else
+          video.setAttribute ('src','https://www.youtube.com/embed/h3uBr0CCm58?autoplay=1');
+
+}
+function out2(){
+        incorrect = incorrect + 1;
+        dispi.style.display = 'block';
+        window.addEventListener('click', outsideClick5);
+        modal.style.display = "none";
+        var mytry = video.getAttribute('src');
+         if(incorrect === 1) {
+         video.setAttribute ('src','https://www.youtube.com/embed/cVUbpTFkDdo?autoplay=1');
+         }    else if(incorrect ===2) {
+         video.setAttribute ('src','https://www.youtube.com/embed/eVFd46qABi0?autoplay=1');
+        }
+        else
+          video.setAttribute ('src','https://www.youtube.com/embed/t-bq1ScyUGI?autoplay=1');
+}
+
+var result3 = document.getElementById("opt3");
+var result4 = document.getElementById("opt4");
+
+result3.addEventListener('click', out3);
+result4.addEventListener('click', out4);
+
+function outsideClick6(e){
+  if(e.target == Resultr){
+    dispc.style.display = 'none';
+  }
+}
+function outsideClick7(e){
+  if(e.target == Resultw){
+    dispi.style.display = 'none';
+  }
+}
+
+span.onclick = function() {
+    modala.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modala) {
+        modala.style.display = "none";
+    }
+}
+
+function out3(){
+         correct = correct + 1;
+        dispc.style.display = 'block';
+        window.addEventListener('click', outsideClick4);
+        modala.style.display = "none";
+        var mytry = video.getAttribute('src');
+         if(correct === 1) {
+         video.setAttribute ('src','https://www.youtube.com/embed/xz3PbpPR6DY?autoplay=1');
+         }    else if(correct ===2) {
+         video.setAttribute ('src','https://www.youtube.com/embed/0HgNdPOZyvQ?autoplay=1');
+        }
+        else
+          video.setAttribute ('src','https://www.youtube.com/embed/h3uBr0CCm58?autoplay=1');
+}
+function out4(){
+        incorrect = incorrect + 1;
+        dispi.style.display = 'block';
+        window.addEventListener('click', outsideClick5);
+        modala.style.display = "none";
+        var mytry = video.getAttribute('src');
+         if(incorrect === 1) {
+         video.setAttribute ('src','https://www.youtube.com/embed/cVUbpTFkDdo?autoplay=1');
+         }    else if(incorrect ===2) {
+         video.setAttribute ('src','https://www.youtube.com/embed/eVFd46qABi0?autoplay=1');
+        }
+        else
+          video.setAttribute ('src','https://www.youtube.com/embed/t-bq1ScyUGI?autoplay=1');
+}
+
+var result5 = document.getElementById("opt5");
+var result6 = document.getElementById("opt6");
+
+result5.addEventListener('click', out5);
+result6.addEventListener('click', out6);
+
+function outsideClick6(e){
+  if(e.target == Resultr){
+    dispc.style.display = 'none';
+  }
+}
+function outsideClick7(e){
+  if(e.target == Resultw){
+    dispi.style.display = 'none';
+  }
+}
+
+span.onclick = function() {
+    modalb.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modalb) {
+        modalb.style.display = "none";
+    }
+}
+
+function out5(){
+        correct = correct + 1;
+        dispc.style.display = 'block';
+        window.addEventListener('click', outsideClick4);
+        modalb.style.display = "none";
+        var mytry = video.getAttribute('src');
+         if(correct === 1) {
+         video.setAttribute ('src','https://www.youtube.com/embed/xz3PbpPR6DY?autoplay=1');
+         }    else if(correct ===2) {
+         video.setAttribute ('src','https://www.youtube.com/embed/0HgNdPOZyvQ?autoplay=1');
+        }
+        else
+          video.setAttribute ('src','https://www.youtube.com/embed/h3uBr0CCm58?autoplay=1');
+}
+function out6(){
+        incorrect = incorrect + 1;
+        dispi.style.display = 'block';
+        window.addEventListener('click', outsideClick5);
+        modalb.style.display = "none";
+        var mytry = video.getAttribute('src');
+         if(incorrect === 1) {
+         video.setAttribute ('src','https://www.youtube.com/embed/cVUbpTFkDdo?autoplay=1');
+         }    else if(incorrect ===2) {
+         video.setAttribute ('src','https://www.youtube.com/embed/eVFd46qABi0?autoplay=1');
+        }
+        else
+          video.setAttribute ('src','https://www.youtube.com/embed/t-bq1ScyUGI?autoplay=1');
+}
