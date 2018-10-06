a18ec52c6e33d5c6ec8a5184e237b8952a99950f
Chokerino <43617111+Chokerino@users.noreply.github.com>
2018-10-06 05:48:47 +0530

    HACK 6

--- main.js
+++ main.js
@@ -1,23 +1,57 @@
 var instruct = document.getElementById("insBtn");
 var pop = document.getElementById("Pg");
 
+var meme1 = document.getElementById("meme1");
+var pop1 = document.getElementById("memee");
+
+var meme2 = document.getElementById("meme2");
+var pop2 = document.getElementById("memef");
+
+var meme3 = document.getElementById("meme3");
+var pop3 = document.getElementById("memeg");
+
 instruct.addEventListener('click', rules);
 window.addEventListener('click', outsideClick);
+
+meme1.addEventListener('click', rules1);
+meme2.addEventListener('click', rules2);
+meme3.addEventListener('click', rules3);
 
 function outsideClick(e){
   if(e.target == Pg){
     pop.style.display = 'none';
   }
 }
+function outsideClick1(e){
+  if(e.target == memee){
+    pop1.style.display = 'none';
+  }
+}
+function outsideClick2(e){
+  if(e.target == memef){
+    pop2.style.display = 'none';
+  }
+}
+function outsideClick3(e){
+  if(e.target == memeg){
+    pop3.style.display = 'none';
+  }
+}
+
 function rules(){
         pop.style.display = 'block';
         window.addEventListener('click', outsideClick);
 }
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-7243260-2']);
-_gaq.push(['_trackPageview']);
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+function rules1(){
+        pop1.style.display = 'block';
+        window.addEventListener('click', outsideClick1);
+}
+function rules2(){
+        pop2.style.display = 'block';
+        window.addEventListener('click', outsideClick2);
+}
+function rules3(){
+        pop3.style.display = 'block';
+        window.addEventListener('click', outsideClick3);
+}
+
